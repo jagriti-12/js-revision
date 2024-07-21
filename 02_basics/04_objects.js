fdb717531c3658f4720d81b2b71a63a3335b6459
Jagriti <94058744+jagriti-12@users.noreply.github.com>
2024-07-21 05:57:34 +0000

    #learning

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -36,4 +36,17 @@ const obj3 = {obj1, obj2}
 console.log(obj3); //{ obj1: { '1': 'A', '2': 'B' }, obj2: { '3': 'C', '4': 'D' } }
 console.log(Object.assign(obj1, obj2)); // { 1: 'A', 2: 'B', 3: 'C', 4: 'D' } //Values are going into obj1
 // to make the values into new one add {} these before obj1, obj2.
-console.log(obj1);
+// {} => This is used because it will act as target and rest all the sources. and all the values form the sources will be added into {} this empty set not in any other.
+console.log(obj1); //Checking that obj2 elements are in in obj1 now.
+// To Remove this redundancy we can have {} this.
+
+const A1 = {1: "J", 2: "S"}
+const A2 = {3: "P", 3: "K"}
+console.log("Elements on combining A1 and A2:");
+console.log(Object.assign({}, A1, A2)); //Now all the values of A1 and A2 will be added in new one instead of A1
+console.log("Elements in A1 after combining:", A1);
+
+// Spread
+const A3 = {...A1,...A2}
+console.log(A3);
+
